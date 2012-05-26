f971c328fd124f4fbad2bd4cd631cd0eda5e5fd6
Charlie Greene <greeneca@gmail.com>
2012-05-25 22:20:41 -0300

    initial commit

---
+++ install.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# install needed programs
+sudo apt-get install vim git ssh zsh tmux
+
+# setup vim dirs
+mkdir ~/.vim
+mkdir ~/.vim/backup
+mkdir ~/.vim/tmp
+
+# move .vimrc
+
+# move .tmux.config
+
+# move .gitconfig
+
+# setup ssh dir
+mkdir ~/.ssh
+
+# move ssh config
+
+# get oh-my-zsh
+
+# move .zshrc
